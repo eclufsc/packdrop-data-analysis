c1bd7273de4e974572d325cf775d05e839f3ba00
vinicius <vinimmbb@gmail.com>
2018-05-21 11:48:24 -0300

    Corrected Result parser

--- icpp2018/experiments/result-parser.py
+++ icpp2018/experiments/result-parser.py
@@ -2,15 +2,14 @@ from experimentaux import *
 
 def g5k_files(wrapper):
   g5k_lbs = ['dist', 'greedy', 'nolb', 'pdlb', 'refine']
-  
-  #wrapper.set_g5k_files('lbtest', ['mesh2d', 'mesh3d', 'ring'], g5k_lbs)
+  wrapper.set_g5k_files('lbtest', ['mesh2d', 'mesh3d', 'ring'], g5k_lbs)
   #wrapper.set_g5k_files('leanmd', ['short', 'long'], g5k_lbs)
+  wrapper.set_sdumont_freq_files(['30', '60'], ['dist', 'pdlb', 'refine', 'dist'])
 
 def sdumont_files(wrapper):
   wrapper.set_sdumont_initial_files('all', ['144', '240', '336', '384', '480', '576', '672', '768'])
   #wrapper.set_sdumont_initial_files('leanmd', ['288', '576', '1142'])
   wrapper.set_sdumont_initial_files('lbtest', ['1142', '1728'])
-  #wrapper.set_sdumont_freq_files(['30', '60'], ['dist', 'pdlb', 'refine', 'dist'])
 
 file_map_func = {
   'g5k': g5k_files,
